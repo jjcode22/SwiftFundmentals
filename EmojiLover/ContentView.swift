25907b9c228762e858383bb7de08d08a05ca7d20
jjcode22 <jubaljacob@Jubals-Air.bbrouter>
2024-01-20 17:59:58 +0530

    Emoji picker

---
+++ EmojiLover/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  EmojiLover
+//
+//  Created by Jubal Jacob on 20/01/24.
+//
+
+import SwiftUI
+
+enum Emoji: String, CaseIterable{
+    case 😂,🤣,👅,😍;
+}
+
+struct ContentView: View {
+    @State var selection: Emoji = .😂
+    var body: some View {
+        VStack {
+            
+            Text("Choose your emoji").font(.system(size: 40)).fontWeight(.bold)
+            Text(selection.rawValue).font(.system(size: 150))
+            
+            Picker("Select Emoji", selection: $selection){
+                ForEach(Emoji.allCases, id: \.self){
+                    emoji in Text(emoji.rawValue)
+                }
+            }
+            .pickerStyle(.segmented)
+        }
+        .navigationTitle("Emoji Lovers!")
+        .padding(16)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Jubal Jacob on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
